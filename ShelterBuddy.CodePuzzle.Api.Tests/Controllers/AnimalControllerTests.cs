f64d441e3b74d6b6097f5f26b233ffe82b408c32
Guilherme Reis <guih2127@gmail.com>
2022-12-04 03:21:33 -0300

    Minor changes on tests

--- ShelterBuddy.CodePuzzle.Api.Tests/Controllers/AnimalControllerTests.cs
+++ ShelterBuddy.CodePuzzle.Api.Tests/Controllers/AnimalControllerTests.cs
@@ -170,7 +170,6 @@ public class AnimalControllerTests
         var controller = new AnimalController(repository);
         var result = controller.Post(data);
 
-        result.ShouldBeOfType<OkObjectResult>();
-        //TODO - TESTAR OBJETO RETORNADO
+        result.ShouldBeOfType<OkObjectResult>();        
     }
 }
